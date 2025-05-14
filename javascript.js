5bd2cfe5145aa7ce5fc2335ea6484c88bfc57e1e
Hasinjo <davidhasinjo@gmail.com>
2025-05-14 13:47:33 +0300

    io ny fichier javascript

---
+++ javascript.js
@@ -0,0 +1,16 @@
+//let nom=prompt("Etrer votre nom");
+//document.getElementById("bjr2").textContent=nom;
+//function f(x){
+//    console.log(x+1);
+//}
+//f(1);
+
+function entrer(){
+    nombre1=prompt("Entrer nombre 1");
+    console.log(nombre1);
+    console.log(typeof(nombre1));
+    nb1=parseFloat(nombre1);
+    console.log(typeof(nb1));
+
+}
+entrer();
